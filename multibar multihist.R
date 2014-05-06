e939df95d5fcc13b38067ab9cfbe0ce9f928f4eb
lillion <lillion@lillions-mbp.fritz.box>
2014-05-06 17:58:32 +0200

    first commit

---
+++ multibar multihist.R
@@ -0,0 +1,60 @@
+
+multihist <- function(x, variables, labels = TRUE, xlab = "Verteilung",save=TRUE,...) {
+	if(class(x)!="data.frame") stop("x is not a Dataframe")
+	if(missing(variables)) variables=1:length(x)
+	ll <- attributes(x)$variable.labels
+	if(length(ll)==0) {
+		cat("no Labels in Dataframe, using variable names instead")
+		ll <- names(x)
+		}
+	ll <- gsub("\\.", "", ll)
+	ll <- sub("\\b(\\w)", "\\U\\1", ll, perl=TRUE)
+	for (i in variables) {
+		if (is.numeric(x[[i]])) {
+			hist(x[[i]], main = ll[i], xlab = xlab, cex.main = 0.8,...)
+				if (save) 
+			dev.copy2pdf(file = paste0(ll[[i]], " (Histogramm).pdf"), height = 8, 
+				width = 12)
+				}
+	}
+}
+
+
+multihist(dd)
+
+multibar(dd, 80:82, save = T)
+multibar(x2,c(1:3))
+
+
+multibar <- function(x, variables, labels, xlab = "Antworten", 
+	save = TRUE,...) {
+	if(class(x)!="data.frame") stop("x is not a Dataframe")
+	if(missing(variables)) variables=1:length(x)
+	if (!missing(labels)) { if(is.character(labels) & length(variables)==length(labels)) {
+		#print("hello")
+		ll <- labels
+	} else {
+		cat("Labels don't correspond to variables\n")
+		ll <- attributes(x)$variable.labels}} else {
+	ll <- attributes(x)$variable.labels
+	}
+	if(length(ll)==0) {
+		cat("no Labels in Dataframe, using variable names instead\n")
+		ll <- names(x)
+		}
+		
+	ll <- gsub("\\.", "", ll)
+	ll <- gsub("\\/","-",ll) # notfalls raus
+	ll <- sub("\\b(\\w)", "\\U\\1", ll, perl=TRUE)
+	for (i in variables) {
+		labelnum <- which(i==variables)
+		barplot(table(x[[i]]), main = ll[labelnum], xlab = xlab, cex.main = 0.8, ...)
+		if (save) 
+			dev.copy2pdf(file = paste0(ll[[labelnum]], " (Barplot).pdf"), height = 8, 
+				width = 12,family="URWPalladio")
+	}
+	#if (save) dev.off()
+}
+
+
+multibar(neo_n,5,ylab="häu")
